eec3c52c4d8f4fd85b14ed30f8bc922926d091c3
Geoffrey GIROU <geoffrey.girou@digilab12.fr>
2023-07-23 17:45:00 +0200

    chore: add pinia store manager and create trek store

---
+++ components/landing/HeroTrekSection.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useTreksStore } from "~/stores/treks";
+
+const store = useTreksStore();
+</script>
+<template>
+  <section id="hero-trek" class="flex flex-col items-center">
+    <h1
+      ref="headingRef"
+      class="inline-flex flex-col items-center mt-12 text-center"
+    >
+      Les plus beaux treks de France
+    </h1>
+    <p class="max-w-2xl mb-12 text-center text-grey-700">
+      Découvrez les parcours de Trek sur le territoire. Explorez les joyaux de
+      vos régions.
+    </p>
+
+    <DsfrButton
+      :label="`Voir les Treks (${store.trekList.length})`"
+      @click="$router.push('/treks')"
+    />
+    <DsfrPicture
+      size="small"
+      alt="Des panoramas à couper le souffle"
+      src="/assets/img/trek-viewpoint.png"
+      ratio="16x9"
+    />
+  </section>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
   css: ['@gouvfr/dsfr/dist/dsfr.min.css', '@gouvminint/vue-dsfr/styles'],
   components: [
     {
@@ -14,4 +15,11 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
 })
