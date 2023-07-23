00c31bede5a8539a86f86a49ce32b734c128e6d3
Geoffrey GIROU <geoffrey.girou@digilab12.fr>
2023-07-23 17:10:15 +0200

    chore: add dsfr plugin and create layout

---
+++ layouts/DsfrLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const navItems = [
+  {
+    to: { name: 'index' },
+    text: 'Accueil',
+  },
+]
+</script>
+
+<template>
+  <div>
+    <DsfrHeader :service-title="`trek.gouv.fr`" />
+    <div class="fr-container">
+      <DsfrNavigation :nav-items="navItems" />
+      <slot />
+    </div>
+    <DsfrFooter />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <DsfrLayout> </DsfrLayout>
+</template>
