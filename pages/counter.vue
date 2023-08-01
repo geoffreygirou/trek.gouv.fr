be6a2c8e23644c1e8eab701a60a585c4ef2866bf
Geoffrey GIROU <geoffrey.girou@gmail.com>
2023-08-01 17:44:43 +0200

    feat: add server side rendering example for fun

---
+++ pages/counter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { pending = true, data: counter } = useLazyFetch('/api/counter', {
+  pick: ['count'],
+})
+</script>
+
+<template>
+  <div v-if="pending">Loading ...</div>
+  <div v-else>
+    <div>Page visits: {{ counter.count }}</div>
+  </div>
+</template>
